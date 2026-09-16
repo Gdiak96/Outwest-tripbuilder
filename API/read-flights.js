5373abc24a662d83fd8397067cfbc3a7e22f945e
Gdiak96 <yiannisdiak@gmail.com>
2026-09-16 23:22:48 +0300

    Refactor flights API response for GET method

--- API/read-flights.js
+++ API/read-flights.js
@@ -1,16 +1,6 @@
 export default function handler(req, res) {
-  if (req.method === "GET") {
-    return res.status(200).send("OK");
-  }
-
-  if (req.method === "POST") {
-    return res.status(200).json({
-      success: true,
-      message: "Flights API is working"
-    });
-  }
-
-  return res.status(405).json({
-    error: "Method not allowed"
+  return res.status(200).json({
+    success: true,
+    message: "Out West Flights API is working"
   });
 }
